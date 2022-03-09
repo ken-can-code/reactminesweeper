c8b5b0169198a967ce0688d894893ad00f2499ac
ken-can-code <gxcadr@gmail.com>
2022-03-09 13:51:17 -0800

    trying to get fixed number of mines to randomly be assigned to squares as props and set their state

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,41 @@ import Square from './Square';
 
 function App() {
   const squares = [];
-  for (let i = 0; i < 100; i += 1) {
-    squares.push(<Square id={`square${i}`} />);
-
+  let totalMines = 30;
+  while (totalMines > 0) {
+    squares[Math.floor(Math.random() * squares.length)]
   }
+
+  for (let i = 0; i < 100; i += 1) {
+    squares.push(<Square id={`square${i}`} isMine={false} />);
+  }
+  console.log('square at position 0 raw boolean value', squares[0].props.isMine);
+
 
   return (
     <div>
-      <p class='section-title'>Minesweeper</p>
-      <div class='board-area'>
-      <div class='board-left' />
-      <div class='board-main'>
-        <div class='board'>
+      <p className='section-title'>A grid</p>
+      <div className='board-area'>
+      <div className='board-left' />
+      <div className='board-main'>
+        <div className='board'>
           {squares} {/* if array is placed in JSX return statement it'll simply display in DOM */}
         </div>
       </div>
-      <div class='board-right'>
+      <div className='board-right'>
         <p id='win'></p>
       </div>
     </div>
-    <div class='instruction-area'>
+    <div className='instruction-area'>
       <div id='left-child'></div>
-      <p id='next-message'>
+      <div id='next-message'>
         Start over: <p id='current-turn'></p>
-      </p>
-      <div class='btn'>
+      </div>
+      <div className='btn'>
         <button id='clear'>Clear</button>
       </div>
     </div>
-    <p class='under-text' id='click-or-tap'>
+    <p className='under-text' id='click-or-tap'>
       Click or tap on the above board to play a move!
     </p>
     <script src='index.jsx'></script>
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Square = () => {
+const Square = (props) => {
+  const {isMine: mineState} = props;
+// state at any given time || update function 
+  const [isMine, setIsMine] = useState(mineState);
+//                         || React function to create the hook || initial state
+
   return (
-    <div class='board-square'> {/* square9 is temporary */}
+    <div class='board-square'>
       <div class='square-contents'></div>
     </div>
   )
 }
 
 export default Square;
+
+// assign a fixed number of mines to grid completely randomly
+
+// keep track of total number of mines to assign
+// e.g. let numOfMines = 30;
+// loop through squares in random order and assign mine to square if not already have mine
+// continue loop until no more mines
